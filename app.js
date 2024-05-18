8c7df0b6204e0abbc331e62d3ef9d2fe2f58f925
judoyo <judoyo@gmail.com>
2024-05-18 15:34:23 +0900

    commit test

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(express.urlencoded({ extended: false }));
-
+1
 app.use(shopRoutes);
 app.use('/admin', adminRoutes);
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page Not found</h1>');
 });
-
+2
 app.listen(3000);
 a
+bcd
+3
