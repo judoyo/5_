77d579b7050d5965145e2b49e9a9920b73c555d3
judoyo <judoyo@gmail.com>
2024-05-21 06:41:20 +0900

    [add] css

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -6,12 +8,13 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(shopRoutes);
 app.use('/admin', adminRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
     // console.log('In another middlewaqre!');
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 
